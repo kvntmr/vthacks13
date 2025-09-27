c7a89c4723c64ea77b181c0ba14fe81bbd4c8444
kvntmr <kevin.tamir07@gmail.com>
2025-09-27 13:17:02 -0400

    spreadsheet demo

---
+++ front/new/app/spreadsheet-demo/page.tsx
@@ -0,0 +1,9 @@
+import { EnhancedSpreadsheetEditor } from "@/components/enhanced-spreadsheet-editor";
+
+export default function SpreadsheetDemoPage() {
+  return (
+    <div className="h-screen">
+      <EnhancedSpreadsheetEditor />
+    </div>
+  );
+}
